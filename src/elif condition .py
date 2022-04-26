05434934e8273b6f024fa5b21a06ca3e1f213090
Prashant9597 <prashant9597@gmail.com>
2022-04-26 23:48:45 +0530

    program for ifelse condition

---
+++ src/elif condition .py
@@ -0,0 +1,11 @@
+name=input("name? ")
+if name =="name":
+    print("The name entered is ", name)
+elif name == "john":
+    print("the name enterd is" ,name)
+elif name == "july":
+    print("the name enterd is"  ,name)
+elif name == "tom":
+    print("the name enterd is "  ,name)
+else:
+    print("the name is enterd invalid")
---
+++ src/ifelse program.py
@@ -0,0 +1,9 @@
+gender1= "female"
+age= 18
+if gender1=="male":
+    if age==18:
+        print("rajan")
+else:
+    print("is a valid person")
+    print("is invalid person") 
+        
